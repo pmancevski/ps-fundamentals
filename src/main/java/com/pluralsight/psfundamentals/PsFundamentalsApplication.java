fa8c93f29b7d25a6f15877125301f5253f8fca46
pmancevski <pmancevskiwork@gmail.com>
2025-06-24 12:58:58 +0200

    initial

---
+++ src/main/java/com/pluralsight/psfundamentals/PsFundamentalsApplication.java
@@ -0,0 +1,13 @@
+package com.pluralsight.psfundamentals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsFundamentalsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PsFundamentalsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pluralsight/psfundamentals/PsFundamentalsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pluralsight.psfundamentals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsFundamentalsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
